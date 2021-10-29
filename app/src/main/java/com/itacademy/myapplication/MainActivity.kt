ca9fe8f5bd9563a66fdc4e4b7f5128adac89ad1b
Temirlannn <duishobekuulutemirlan0@gmail.com>
2021-10-29 20:28:59 +0600

    Добавили форму регистрации пользвателю

--- app/src/main/java/com/itacademy/myapplication/MainActivity.kt
+++ app/src/main/java/com/itacademy/myapplication/MainActivity.kt
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    var i = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //n
     }
 
 }
