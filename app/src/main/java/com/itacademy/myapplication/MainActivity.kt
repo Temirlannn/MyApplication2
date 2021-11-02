b03017f0fa6fbad562879fc0c920e01fe3f4b6e3
Temirlannn <duishobekuulutemirlan0@gmail.com>
2021-11-02 18:32:14 +0600

    commit

--- app/src/main/java/com/itacademy/myapplication/MainActivity.kt
+++ app/src/main/java/com/itacademy/myapplication/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.itacademy.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var firstName: EditText
+    lateinit var age: EditText
+    lateinit var BirthDate: EditText
+
+    lateinit var saveBtn: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firstName = findViewById(R.id.firstName)
+        age = findViewById(R.id.age)
+        BirthDate = findViewById(R.id.BirthDate)
+        saveBtn = findViewById(R.id.saveBtn)
+
+        saveBtn.setOnClickListener {
+            val name = firstName.text
+            val ageText = age.text
+            val birth = BirthDate.text
+            val intent = Intent(this,SecondActivity::class.java) // intent - путь
+            startActivity(intent)
+
+        }
     }
 
 }
