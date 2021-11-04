133d249f1b3660f1321378ae00e5a5c42995cc63
Temirlannn <duishobekuulutemirlan0@gmail.com>
2021-11-05 02:09:03 +0600

    commit

--- app/src/main/java/com/itacademy/myapplication/MainActivity.kt
+++ app/src/main/java/com/itacademy/myapplication/MainActivity.kt
@@ -3,8 +3,10 @@ package com.itacademy.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,8 +15,6 @@ class MainActivity : AppCompatActivity() {
     lateinit var BirthDate: EditText
 
     lateinit var saveBtn: Button
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +25,43 @@ class MainActivity : AppCompatActivity() {
         saveBtn = findViewById(R.id.saveBtn)
 
         saveBtn.setOnClickListener {
-            val name = firstName.text
-            val ageText = age.text
-            val birth = BirthDate.text
+            val name = firstName.text.toString()
+            val ageText = age.text.toString()
+            val birth = BirthDate.text.toString()
             val intent = Intent(this,SecondActivity::class.java) // intent - путь
+
+            intent.putExtra("firstname", name)
+            intent.putExtra("age", ageText)
+            intent.putExtra("BirthDate", birth)
             startActivity(intent)
 
         }
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.i("OnResume!!!", "ВОСПРОИЗВЕДЕНИЕ!!!")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("OnStart!!!", "СТАРТ!!!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("OnPause!!!","ПАУЗА!!!")
+        Toast.makeText(this, "onPause!!!", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("OnStop!!!","СТОП!!!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("OnDestroy","Уничтожение")
     }
 
 }
--- app/src/main/java/com/itacademy/myapplication/SecondActivity.kt
+++ app/src/main/java/com/itacademy/myapplication/SecondActivity.kt
@@ -2,10 +2,19 @@ package com.itacademy.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val  firstName = intent.getStringExtra("firstName")
+        val  age= intent.getStringExtra("age")
+        val  BirthDate = intent.getStringExtra( "BirthDate")
+        Log.i("FirstName", firstName!!)
+        Log.i("age", age!!)
+        Log.i("BirthDate", BirthDate!!)
+
     }
 }
